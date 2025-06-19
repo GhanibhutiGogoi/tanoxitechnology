8939362c9331bfa98858dee84e768b2bbb39c4c7
Ghanibhuti Gogoi <144294876+GhanibhutiGogoi@users.noreply.github.com>
2025-06-20 01:02:24 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Tanoxi-Technology' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Tanoxi-Technology/' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
+  trailingSlash: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
