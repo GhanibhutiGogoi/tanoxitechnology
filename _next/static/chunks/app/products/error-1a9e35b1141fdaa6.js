46e0c01d8639a3bd083d94b14dd58a8a161f3153
GhanibhutiGogoi <144294876+GhanibhutiGogoi@users.noreply.github.com>
2025-06-19 17:03:56 +0000

    Deploying to gh-pages from @ GhanibhutiGogoi/tanoxitechnology@161d3a37c0f25ca7caeedb8a5e55738a5020b189 🚀

---
+++ _next/static/chunks/app/products/error-1a9e35b1141fdaa6.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[833],{1466:function(e,r,n){Promise.resolve().then(n.bind(n,3678))},3678:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return Error}});var t=n(7437);function Error(e){let{error:r,reset:n}=e;return(0,t.jsxs)("div",{className:"flex flex-col items-center justify-center min-h-screen",children:[(0,t.jsx)("h2",{className:"text-2xl font-bold mb-4",children:"Something went wrong!"}),(0,t.jsx)("button",{onClick:()=>n(),className:"bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark",children:"Try again"})]})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),i=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,f={},u=null,l=null;for(t in void 0!==n&&(u=""+n),void 0!==r.key&&(u=""+r.key),void 0!==r.ref&&(l=r.ref),r)s.call(r,t)&&!c.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:u,ref:l,props:f,_owner:i.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=1466)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/products/loading-b26caebe89fa0e94.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[402],{6252:function(e,r,n){Promise.resolve().then(n.bind(n,5563))},5563:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return Loading}});var t=n(7437);function Loading(){return(0,t.jsx)("div",{className:"flex justify-center items-center min-h-screen",children:(0,t.jsx)("div",{className:"animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"})})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),i=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,f={},c=null,a=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(a=r.ref),r)s.call(r,t)&&!u.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:a,props:f,_owner:i.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=6252)}),_N_E=e.O()}]);
