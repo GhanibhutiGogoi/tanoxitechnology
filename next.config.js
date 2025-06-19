3f5d48578fd859cfc048e4d6b99f69177030754f
Ghanibhuti Gogoi <144294876+GhanibhutiGogoi@users.noreply.github.com>
2025-06-20 00:57:02 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.NODE_ENV === 'production' ? {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/Tanoxi-Technology',
-    assetPrefix: '/Tanoxi-Technology'
-  } : {
-    // Development configuration
-    images: {
-      unoptimized: true,
-    }
-  })
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/Tanoxi-Technology' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Tanoxi-Technology/' : '',
 }
 
 module.exports = nextConfig
