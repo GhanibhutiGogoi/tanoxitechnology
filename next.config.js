113c7bc8baa12cbb8d4208c7a4f99e991203deb8
Ghanibhuti Gogoi <144294876+GhanibhutiGogoi@users.noreply.github.com>
2025-06-20 11:17:50 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './src/utils/imageLoader.ts',
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
   trailingSlash: true,
-  reactStrictMode: true,
 }
 
 module.exports = nextConfig
